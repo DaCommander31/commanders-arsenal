c668d8e9f276444db805a0cf1178e61cf6d8c676
DaCommander31 <millerjr808@gmail.com>
2025-09-21 15:39:39 +1000

    Initial Commit

---
+++ src/main/java/dev/dacommander31/cmda/CommandersArsenalClient.java
@@ -0,0 +1,23 @@
+package dev.dacommander31.cmda;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+@Mod(value = CommandersArsenal.MOD_ID, dist = Dist.CLIENT)
+@EventBusSubscriber(modid = CommandersArsenal.MOD_ID, value = Dist.CLIENT)
+public class CommandersArsenalClient {
+    public CommandersArsenalClient(ModContainer container) {
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+
+    }
+}
